066339b05df0a862c5fc266b9f865ec1ac4683c4
kettoleon <kettoleon@gmail.com>
2022-11-06 18:36:47 +0100

    Added better random fake data for local development

--- src/main/java/com/github/kettoleon/sweepstakes/configuration/LocalDevelopmentConfiguration.java
+++ src/main/java/com/github/kettoleon/sweepstakes/configuration/LocalDevelopmentConfiguration.java
@@ -2,6 +2,9 @@ package com.github.kettoleon.sweepstakes.configuration;
 
 import com.github.javafaker.Faker;
 import com.github.kettoleon.sweepstakes.bet.repo.BetsRepository;
+import com.github.kettoleon.sweepstakes.bet.repo.FixtureBet;
+import com.github.kettoleon.sweepstakes.league.model.Fixture;
+import com.github.kettoleon.sweepstakes.league.model.LeagueProvider;
 import com.github.kettoleon.sweepstakes.users.repo.User;
 import com.github.kettoleon.sweepstakes.users.repo.UserRepository;
 import org.slf4j.Logger;
@@ -12,6 +15,11 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
 
 @Configuration
 @ConditionalOnProperty(name = "localdev", havingValue = "true")
@@ -28,14 +36,44 @@ public class LocalDevelopmentConfiguration {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private LeagueProvider leagueProvider;
+
     @EventListener(ApplicationReadyEvent.class)
     public void addRandomUsersAndBetsIfNeeded() {
+        Random r = new Random();
         if (userRepository.findAll().isEmpty()) {
+            List<User> users = new ArrayList<>();
             for (int i = 0; i < 50; i++) {
                 User user = createUser(i);
-                log.info("    - Creating user: " + user.getEmail());
-                userRepository.save(user);
+                if (r.nextInt(0, 100) > 95) {
+                    user.setContact(true);
+                }
+                users.add(user);
+                log.info("    - Created user: " + user.getEmail());
             }
+            userRepository.saveAllAndFlush(users);
+            log.info("    - Done creating users");
+        }
+        if (betsRepository.findAll().isEmpty()) {
+            log.info("    - Creating everyone's bets...");
+            for (User user : userRepository.findAll()) {
+                List<FixtureBet> bets = new ArrayList<>();
+                for (Fixture fixture : leagueProvider.getLeague().getFixtures()) {
+                    FixtureBet bet = new FixtureBet();
+                    bet.setId(UUID.randomUUID().toString());
+                    bet.setFixtureId(fixture.getId());
+                    bet.setEmail(user.getEmail());
+                    bet.setHome(r.nextInt(0, 6));
+                    bet.setAway(r.nextInt(0, 6));
+                    if (r.nextInt(0, 100) > 1) {
+                        bets.add(bet);
+                    }
+                }
+
+                betsRepository.saveAllAndFlush(bets);
+            }
+            log.info("    - Done creating everyone's bets");
         }
     }
 
