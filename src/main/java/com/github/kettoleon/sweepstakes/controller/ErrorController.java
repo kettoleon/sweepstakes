454d2c6efbc1605f97809738ab7348d1457bde7f
enr02 <enric.rodriguez@sky.uk>
2022-11-07 18:06:42 +0100

    Adding a basic custom error page

---
+++ src/main/java/com/github/kettoleon/sweepstakes/controller/ErrorController.java
@@ -0,0 +1,40 @@
+package com.github.kettoleon.sweepstakes.controller;
+
+import com.github.kettoleon.sweepstakes.league.model.League;
+import com.github.kettoleon.sweepstakes.league.model.LeagueProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
+
+    @Autowired
+    private LeagueProvider leagueProvider;
+
+    @RequestMapping("/error")
+    public ModelAndView handleError(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+            Integer statusCode = Integer.valueOf(status.toString());
+            return page("error", "Oops! This is so embarrassing...")
+                    .addObject("statusCode", statusCode);
+        }
+        return page("error", "Oops! This is so embarrassing...");
+    }
+
+    public ModelAndView page(String viewId, String title) {
+        ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("page", viewId);
+        modelAndView.addObject("pageTitle", title);
+        League league = leagueProvider.getLeague();
+        modelAndView.addObject("league", league);
+        return modelAndView;
+    }
+}
